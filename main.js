d3e235ad180fbb7265f7e01ace988b1e50101d0c
MatteoZattini <matteozattini@hotmail.com>
2024-05-03 18:55:41 +0200

    m4 possibile soluzione

--- main.js
+++ main.js
@@ -6,6 +6,36 @@ let NumeriUtente = []
 arrayRng()
 ContenitoreNumero.innerHTML = NumeriComputer;
 
+setTimeout(function(){
+    ContenitoreNumero.classList.add("d-none")
+    while (NumeriUtente.length <= 5){
+        let UserInput = prompt("scegli i tuoi numeri")
+        NumeriUtente.push(parseInt(UserInput))
+    }
+    console.log(NumeriUtente)
+    
+    // if(NumeriComputer.toString === NumeriUtente.toString) {
+    //     alert("hai vinto")
+    // } else {
+    //     alert("hai perso")
+    // }
+    const compareArrays = (a, b) =>
+        a.length === b.length &&
+        a.every((element, index) => element === b[index]);
+      
+      console.log(compareArrays(NumeriComputer, NumeriUtente));
+
+    
+}, 3000);
+
+  
+
+
+
+
+
+
+
 
 
 function arrayRng () {
@@ -18,14 +48,3 @@ function arrayRng () {
     return NumeriComputer
 }
 
-setTimeout(function(){
-    ContenitoreNumero.classList.add("d-none")
-    while (NumeriUtente.length <= 5){
-        let UserInput = prompt("scegli i tuoi numeri")
-        NumeriUtente.push(UserInput)
-    }
-
-    console.log(NumeriUtente)
-    return NumeriUtente
-
-}, 3000);
